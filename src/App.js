3d75371f28eb5653e3e2d751523c81e15ccabee8
donPabloHDF <donpabloeldefelicitas@gmail.com>
2022-10-26 18:52:00 +0200

    Final

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ function App() {
     <div className="container-md p-5">
       
           <div className='container p-0 m-0 d-flex flex-column bg-light shadow'>
-              <div className='d-flex justify-content-center'><h1 className='fw-lighter'>todos</h1></div>
+{/* TITULO *********************************************************************************/}
+<header className='d-flex justify-content-center'><h1 className='fw-lighter'>todos</h1></header>
+{/* FORMULARIO *********************************************************************************/}
               <div className='d-flex justify-content-center'>
                   <form onSubmit={handleSubmit(onSubmit)} >
                     <div className="container p-2 m-0 d-flex flex-row">
@@ -40,12 +42,11 @@ function App() {
                     </div>
                     {errors.newTodo && <span className="text-danger text-small d-block m-2 fw-lighter">El campo no puede estar vacío</span>}
                   </form>
-                  
               </div>
+{/* DATOS****************************************/}
               <div className="d-flex justify-content-center flex-column mb-3">
-                
                 {
-                    datos.map((item, index) =>
+                    datos.map((item) =>
                       <div className="p-3 d-flex align-items-center justify-content-between border-bottom border-2" key={item.id}  >
                         <h5 className='p-0 m-0 fw-lighter'>{item.newTodo}</h5>
                         <button type="button" className="btn-close" aria-label="Close" onClick={()=>deleteTodo(item)}></button>
